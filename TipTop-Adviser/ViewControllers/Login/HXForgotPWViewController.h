5b7820c0b9cbed6e0d1d0cfafa18e35c265cf3ff
RockerHX <RockerHX@gmail.com>
2015-10-07 15:46:00 +0800

    新增：
    1.登录模块-忘记密码页面素材资源
    2.登录模块-忘记密码页面布局
    3.登录模块-登录页面更新

---
+++ TipTop-Adviser/ViewControllers/Login/HXForgotPWViewController.h
@@ -0,0 +1,15 @@
+//
+//  HXForgotPWViewController.h
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/7.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXForgotPWViewController : UITableViewController
+
+- (IBAction)backButtonPressed;
+
+@end
---
+++ TipTop-Adviser/ViewControllers/Login/HXForgotPWViewController.m
@@ -0,0 +1,28 @@
+//
+//  HXForgotPWViewController.m
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/7.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import "HXForgotPWViewController.h"
+
+@interface HXForgotPWViewController ()
+
+@end
+
+@implementation HXForgotPWViewController
+
+#pragma mark - View Controller Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Event Response
+- (IBAction)backButtonPressed {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end

4973868757b95a33f2467059db58812971ede565
RockerHX <RockerHX@gmail.com>
2015-10-07 11:37:26 +0800

    初始化：
    1.工程

---
+++ Podfile
@@ -0,0 +1,27 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+pod 'ReactiveCocoa'
+pod 'BlocksKit'
+pod 'Aspects'
+
+pod 'AFNetworking'
+pod 'AFNetworkActivityLogger'
+pod 'SDWebImage'
+pod 'MJExtension'
+
+pod 'MBProgressHUD'
+pod 'MJRefresh'
+pod 'UITableView+FDTemplateLayoutCell'
+
+pod 'HexColors'
+pod 'DateTools'
+pod 'IQKeyboardManager'
+pod 'Masonry'
+
+pod 'JZNavigationExtension'
+
+# ********************** 第三方SDK **********************
+# 百度地图SDK
+#pod 'BaiduMap-iOS-SDK'
+pod 'BaiduMapAPI'
---
+++ TipTop-Adviser/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/7.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+

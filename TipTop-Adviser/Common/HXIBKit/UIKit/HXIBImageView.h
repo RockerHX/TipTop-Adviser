8d32696c0b721441bb51b3ed423cfe61c8532e64
RockerHX <RockerHX@gmail.com>
2015-10-13 22:47:05 +0800

    更新：
    1.IB实时刷新类UIImageView

---
+++ TipTop-Adviser/Common/HXIBKit/UIKit/HXIBImageView.h
@@ -0,0 +1,14 @@
+//
+//  HXIBImageView.h
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/13.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface HXIBImageView : UIImageView
+
+@end
---
+++ TipTop-Adviser/Common/HXIBKit/UIKit/HXIBImageView.m
@@ -0,0 +1,13 @@
+//
+//  HXIBImageView.m
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/13.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import "HXIBImageView.h"
+
+@implementation HXIBImageView
+
+@end

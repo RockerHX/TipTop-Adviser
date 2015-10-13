a22fadce5b3989fe410d081ebbf1744c17d1b884
RockerHX <RockerHX@gmail.com>
2015-10-14 00:48:10 +0800

    更新：
    1.登录模块资源文件名
    2.登录模块UI元素资源文件名
    3.完善修改密码页面UI

---
+++ TipTop-Adviser/ViewControllers/User/Setting/HXChangePassWordViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXChangePassWordViewController.h
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/14.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXChangePassWordViewController : UITableViewController
+
+@end
---
+++ TipTop-Adviser/ViewControllers/User/Setting/HXChangePassWordViewController.m
@@ -0,0 +1,23 @@
+//
+//  HXChangePassWordViewController.m
+//  TipTop-Adviser
+//
+//  Created by ShiCang on 15/10/14.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import "HXChangePassWordViewController.h"
+
+@interface HXChangePassWordViewController ()
+
+@end
+
+@implementation HXChangePassWordViewController
+
+#pragma mark - View Controller Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
